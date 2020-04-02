77ecd9dc3175663747299c5fc0c2373d4a9223e6
Wisam <wisam_mozalbat@hotmail.com>
2020-04-02 04:26:37 -0400

    Carrusel done

--- components/profile/UserImages.js
+++ components/profile/UserImages.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import {View, Text, TouchableOpacity, StyleSheet, Dimensions, Image} from 'react-native';
 
-import { Ionicons } from '@expo/vector-icons'
 
 const UserImages = props => {
+
+    const { images = [] } = props
+
     return (
             <View style={styles.container}>
-                <View style={styles.image}><Text>User images</Text></View>
-                <View style={styles.image}><Text>User images</Text></View>
-                <View style={styles.image}><Text>User images</Text></View>
-                <View style={styles.image}><Text>User images</Text></View>
-                <View style={styles.image}><Text>User images</Text></View>
+                {images ? 
+                    images.map((image, index) => {
+                        return (
+                            <View key={index} style={styles.image}>
+                                <Image
+                                    style={styles.image}
+                                    resizeMode='cover'
+                                    source={{uri: image}}/> 
+                            </View>
+                        )
+                    })
+                    : null
+                }
             </View>
-    )
+        )
 }
 
 
@@ -20,13 +30,15 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        alignItems: 'center'
     },
     image: {
-        width: 120,
-        height:120,
-        margin:5,
+        width: 150,
+        height:150,
         backgroundColor: 'blue',
+        borderRadius: 10,
+        marginRight: 10
     }
 })
 export default UserImages;
