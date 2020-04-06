a252f4d63dd1858ed60032bbade01f08196e7a13
Wisam <wisam_mozalbat@hotmail.com>
2020-04-06 02:49:09 -0400

    Nav update

--- screens/CreateProfileScreen.js
+++ screens/CreateProfileScreen.js
@@ -87,7 +87,7 @@ const CreateProfileScreen = props => {
                     db.collection('profile').add(postProfile).then(ref => {
                         profileContext.setProfile(postProfile)
                         setLoading(false);
-                        props.navigation.replace({routeName: 'Main'})
+                        props.navigation.navigate('Tabs')
                     })
                 })
             
